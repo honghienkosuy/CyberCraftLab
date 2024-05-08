ea3da5b93b997ca2f0c7aedce74b9091932035bf
honghienkosuy <kingarhturnam1311@outlook.com>
2024-05-09 03:00:42 +0700

    hello world

--- index.js
+++ index.js
@@ -1,26 +1,16 @@
-function isValidSudoku(board) {
-  for (let i = 0; i < 9; i++) {
-    const row = new Set();
-    const col = new Set();
-    const box = new Set();
-    for (let j = 0; j < 9; j++) {
-      const rowVal = board[i][j];
-      const colVal = board[j][i];
-      const boxVal =
-        board[3 * Math.floor(i / 3) + Math.floor(j / 3)][3 * (i % 3) + (j % 3)];
-      if (rowVal !== ".") {
-        if (row.has(rowVal)) return false;
-        row.add(rowVal);
-      }
-      if (colVal !== ".") {
-        if (col.has(colVal)) return false;
-        col.add(colVal);
-      }
-      if (boxVal !== ".") {
-        if (box.has(boxVal)) return false;
-        box.add(boxVal);
-      }
-    }
+function reverseBetween(head, m, n) {
+  if (!head || m === n) return head;
+  const dummy = new ListNode(0);
+  dummy.next = head;
+  let prev = dummy;
+  for (let i = 0; i < m - 1; i++) prev = prev.next;
+  let start = prev.next;
+  let then = start.next;
+  for (let i = 0; i < n - m; i++) {
+    start.next = then.next;
+    then.next = prev.next;
+    prev.next = then;
+    then = start.next;
   }
-  return true;
+  return dummy.next;
 }
