50cfa13cd5086dbcac27f0ceead2fb175ef9dd33
honghienkosuy <kingarhturnam1311@outlook.com>
2024-05-09 16:25:39 +0700

    commit

--- index.js
+++ index.js
@@ -1,16 +1,5 @@
-function reverseBetween(head, m, n) {
-  if (!head || m === n) return head;
-  const dummy = new ListNode(0);
-  dummy.next = head;
-  let prev = dummy;
-  for (let i = 0; i < m - 1; i++) prev = prev.next;
-  let start = prev.next;
-  let then = start.next;
-  for (let i = 0; i < n - m; i++) {
-    start.next = then.next;
-    then.next = prev.next;
-    prev.next = then;
-    then = start.next;
-  }
-  return dummy.next;
+function sayHi() {
+  console.log("Hi!");
 }
+
+setTimeout(sayHi, 2000); // Run sayHi after 2 seconds
