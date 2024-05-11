e00318b7ce28015d8d5500d242a0c6de408fab56
honghienkosuy <kingarhturnam1311@outlook.com>
2024-05-12 04:57:53 +0700

    hello world

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-function groupAnagrams(strs) {
-  const map = new Map();
-  for (const str of strs) {
-    const sortedStr = str.split("").sort().join("");
-    if (!map.has(sortedStr)) map.set(sortedStr, []);
-    map.get(sortedStr).push(str);
+function searchMatrix(matrix, target) {
+  if (matrix.length === 0 || matrix[0].length === 0) return false;
+  const rows = matrix.length;
+  const cols = matrix[0].length;
+  let row = 0;
+  let col = cols - 1;
+  while (row < rows && col >= 0) {
+    if (matrix[row][col] === target) return true;
+    else if (matrix[row][col] < target) row++;
+    else col--;
   }
-  return [...map.values()];
+  return false;
 }
