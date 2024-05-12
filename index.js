bd03d5f8780f967c40760d3188c2bf52b65363bb
honghienkosuy <kingarhturnam1311@outlook.com>
2024-05-12 23:20:10 +0700

    update sdk task commit

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-function searchMatrix(matrix, target) {
-  if (matrix.length === 0 || matrix[0].length === 0) return false;
-  const rows = matrix.length;
-  const cols = matrix[0].length;
-  let row = 0;
-  let col = cols - 1;
-  while (row < rows && col >= 0) {
-    if (matrix[row][col] === target) return true;
-    else if (matrix[row][col] < target) row++;
-    else col--;
-  }
-  return false;
-}
+const word = "hello";
+const reversedWord = word.split("").reverse().join("");
+console.log(reversedWord); // olleh
