a5bc3854ea0289e5b87f443ad51e23d2ee16fdff
honghienkosuy <kingarhturnam1311@outlook.com>
2024-05-15 23:13:52 +0700

    feat commit

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-function fibonacci(n) {
-  if (n <= 1) return n;
-  return fibonacci(n - 1) + fibonacci(n - 2);
+function maxProduct(nums) {
+  if (nums.length === 0) return 0;
+  let maxSoFar = nums[0];
+  let minSoFar = nums[0];
+  let maxProduct = nums[0];
+  for (let i = 1; i < nums.length; i++) {
+    const temp = maxSoFar;
+    maxSoFar = Math.max(nums[i], nums[i] * maxSoFar, nums[i] * minSoFar);
+    minSoFar = Math.min(nums[i], nums[i] * temp, nums[i] * minSoFar);
+    maxProduct = Math.max(maxProduct, maxSoFar);
+  }
+  return maxProduct;
 }
